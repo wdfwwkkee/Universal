8e38e0d8d25e316b08d63b783982b1437ac3702e
wdfwwkkee <denis.usmanov101@mail.ru>
2023-09-12 19:47:24 +0500

    desc

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,8 @@
 var swiper = new Swiper(".Hot-Swiper", {
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
